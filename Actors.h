e8368b5f3581f0d55997bab35f11196f3393715c
Ryan-Aguilar <74695989+Ryan-Aguilar@users.noreply.github.com>
2021-05-04 11:01:04 -0700

    Create Actors.h

---
+++ Actors.h
@@ -0,0 +1,50 @@
+#ifndef ACTOR_H_
+#define ACTOR_H_
+
+#include "GraphObject.h"
+
+
+class Actor : public GraphObject {
+public:
+	
+	Actor(int imageID, int startX, int startY, Direction startDirection, float size, unsigned int depth);
+
+
+
+};
+
+class Iceman : public Actor {
+public:
+	Iceman( int startX, int startY, Direction startDirection, float size, unsigned int depth);
+	void attributes(int health, int squirt, int sonarCharge, int gold_sack, Direction facing);
+	void doSomething();
+private:
+	int i_health;
+	int i_squirt;
+	int i_sonarCharge;
+	int i_gold_sack ;
+	Direction i_facing;
+};
+//
+//class Protester : public Actor {
+//public:
+//	Protester(int imageID, int startX, int startY, Direction startDirection, float size = 1.0, unsigned int depth = 0);
+//	
+//};
+//
+//class HardcoreProtester : public Actor {
+//public:
+//	HardcoreProtester(int imageID, int startX, int startY, Direction startDirection, float size = 1.0, unsigned int depth = 0);
+//};
+//
+//
+//class Collectable : public GraphObject {
+//
+//	Collectable(int imageID, int startX, int startY, Direction startDirection, float size = 1.0, unsigned int depth = 0);
+//};
+
+
+
+// Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
+
+#endif // ACTOR_H_
